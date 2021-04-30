4fb5111eb65e8d09c9ac895851710066b3c65a47
Martin Stjernholm <mast@google.com>
2021-04-30 11:53:52 +0100

    Update ART Module prebuilts to build 7326649.
    Taken from branch git_sc-mainline-prod, target mainline_modules-userdebug.
    
    CL prepared by vendor/unbundled_google/modules/ArtGooglePrebuilt/update-art-module-prebuilts.py.
    
    Test: Presubmits
    Bug: 172480615
    Change-Id: I3e4bc3f7e2e8a2c12f07d39f2e0c81ee34d180e2

---
+++ current/sdk/sdk_library/public/legacy.art.module.platform.api_stub_sources/android/system/PacketSocketAddress.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package android.system;
+
+
+/**
+ * Packet socket address.
+ *
+ * Corresponds to Linux's {@code struct sockaddr_ll}.
+ *
+ * See <a href="https://man7.org/linux/man-pages/man7/packet.7.html">packet(7)</a>.
+ *
+ * @hide
+ */
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public final class PacketSocketAddress extends java.net.SocketAddress {
+
+/**
+ * Constructs a new PacketSocketAddress with all the "in" parameters which
+ * correspond to Linux's {@code struct sockaddr_ll}.
+ *
+ * See <a href="https://man7.org/linux/man-pages/man7/packet.7.html">packet(7)</a>.
+ *
+ * @param sll_protocol protocol field in {@code struct sockaddr_ll}
+ * @param sll_ifindex  interface index number field in {@code struct sockaddr_ll}
+ * @param sll_addr     physical-layer address field in {@code struct sockaddr_ll}
+ */
+
+public PacketSocketAddress(int sll_protocol, int sll_ifindex, byte[] sll_addr) { throw new RuntimeException("Stub!"); }
+
+public java.lang.String toString() { throw new RuntimeException("Stub!"); }
+}
+
---
+++ current/sdk/sdk_library/public/legacy.art.module.platform.api_stub_sources/dalvik/system/VMStack.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package dalvik.system;
+
+
+/**
+ * Provides a limited interface to the Dalvik VM stack. This class is mostly
+ * used for implementing security checks.
+ *
+ * @hide
+ */
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public final class VMStack {
+
+private VMStack() { throw new RuntimeException("Stub!"); }
+
+/**
+ * Retrieves an annotated stack trace from the specified thread.
+ *
+ * @param t
+ *      thread of interest
+ * @return an array of annotated stack frames, or null if the thread
+ *      doesn't have a stack trace (e.g. because it exited)
+ */
+
+@dalvik.annotation.optimization.FastNative
+public static native dalvik.system.AnnotatedStackTraceElement[] getAnnotatedThreadStackTrace(java.lang.Thread t);
+}
+
