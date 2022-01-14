0bc24b6b08b10021ad2fcab9bd432dffb3af4ea8
Martin Stjernholm <mast@google.com>
2022-01-14 22:44:06 +0000

    Update ART Module prebuilts to build 8081436.
    Taken from branch aosp-master-art, target mainline_modules_sdks.
    
    CL prepared by packages/modules/ArtPrebuilt/update-art-module-prebuilts.py.
    
    Test: Presubmits
    Bug: 214602476
    Change-Id: I0b58b471225f6612532072e39ad8a49b121e33b9
    Merged-In: I0b58b471225f6612532072e39ad8a49b121e33b9

--- current/sdk/sdk_library/public/art.module.public.api_stub_sources/java/security/acl/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The classes and interfaces in this package have been
- * superseded by classes in the java.security package.
- * See that package and, for example, java.security.Permission for details.
- *
- * @since JDK1.1
- */
-
-package java.security.acl;
