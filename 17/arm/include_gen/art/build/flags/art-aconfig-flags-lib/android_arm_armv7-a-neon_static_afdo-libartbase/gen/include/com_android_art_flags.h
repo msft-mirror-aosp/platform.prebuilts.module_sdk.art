cb91d2977358195042a91013758f96c700e0510f
Linus Tufvesson <lus@google.com>
2025-02-10 13:29:12 +0100

    Finalize artifacts for extension SDK 17
    Import from build id 13034767.
    
    Generated with:
    $ packages/modules/common/tools/finalize_sdk.py -r 'Finalized together with Android 36 (all modules)' -b 393356045 -f 17 13034767
    
    Bug: 393356045
    Test: presubmit
    Change-Id: I7684d318a2b6375db655d46e9bd98845820cb6cf

---
+++ 17/arm/include_gen/art/build/flags/art-aconfig-flags-lib/android_arm_armv7-a-neon_static_afdo-libartbase/gen/include/com_android_art_flags.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#ifndef COM_ANDROID_ART_FLAGS
+#define COM_ANDROID_ART_FLAGS(FLAG) COM_ANDROID_ART_FLAGS_##FLAG
+#endif
+
+#ifndef COM_ANDROID_ART_FLAGS_ALWAYS_ENABLE_PROFILE_CODE
+#define COM_ANDROID_ART_FLAGS_ALWAYS_ENABLE_PROFILE_CODE false
+#endif
+#ifndef COM_ANDROID_ART_FLAGS_ART_SERVICE_V3
+#define COM_ANDROID_ART_FLAGS_ART_SERVICE_V3 true
+#endif
+#ifndef COM_ANDROID_ART_FLAGS_EXECUTABLE_METHOD_FILE_OFFSETS
+#define COM_ANDROID_ART_FLAGS_EXECUTABLE_METHOD_FILE_OFFSETS true
+#endif
+#ifndef COM_ANDROID_ART_FLAGS_FAST_BASELINE_COMPILER
+#define COM_ANDROID_ART_FLAGS_FAST_BASELINE_COMPILER false
+#endif
+#ifndef COM_ANDROID_ART_FLAGS_TEST
+#define COM_ANDROID_ART_FLAGS_TEST true
+#endif
+#ifndef COM_ANDROID_ART_FLAGS_USE_GENERATIONAL_CMC
+#define COM_ANDROID_ART_FLAGS_USE_GENERATIONAL_CMC false
+#endif
+
+#ifdef __cplusplus
+
+#include <memory>
+
+namespace com::android::art::flags {
+
+class flag_provider_interface {
+public:
+    virtual ~flag_provider_interface() = default;
+    virtual bool always_enable_profile_code() = 0;
+    virtual bool art_service_v3() = 0;
+    virtual bool executable_method_file_offsets() = 0;
+    virtual bool fast_baseline_compiler() = 0;
+    virtual bool test() = 0;
+    virtual bool use_generational_cmc() = 0;
+};
+
+extern std::unique_ptr<flag_provider_interface> provider_;
+
+
+inline bool always_enable_profile_code() {
+    return COM_ANDROID_ART_FLAGS_ALWAYS_ENABLE_PROFILE_CODE;
+}
+inline bool art_service_v3() {
+    return COM_ANDROID_ART_FLAGS_ART_SERVICE_V3;
+}
+inline bool executable_method_file_offsets() {
+    return COM_ANDROID_ART_FLAGS_EXECUTABLE_METHOD_FILE_OFFSETS;
+}
+inline bool fast_baseline_compiler() {
+    return COM_ANDROID_ART_FLAGS_FAST_BASELINE_COMPILER;
+}
+inline bool test() {
+    return COM_ANDROID_ART_FLAGS_TEST;
+}
+inline bool use_generational_cmc() {
+    return COM_ANDROID_ART_FLAGS_USE_GENERATIONAL_CMC;
+}
+
+}
+
+extern "C" {
+#endif // __cplusplus
+
+
+bool com_android_art_flags_always_enable_profile_code();
+bool com_android_art_flags_art_service_v3();
+bool com_android_art_flags_executable_method_file_offsets();
+bool com_android_art_flags_fast_baseline_compiler();
+bool com_android_art_flags_test();
+bool com_android_art_flags_use_generational_cmc();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
